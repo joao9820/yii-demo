320c21a99e552ed2d92624077561a777fe909271
joao9820 <joaovictorv9820@gmail.com>
2025-10-27 01:32:55 -0300

    Gerando Models, CRUD e views através do Gii

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -3,32 +3,132 @@
 namespace app\controllers;
 
 use app\models\Country;
-use yii\data\Pagination;
+use app\models\CountrySearch;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
-class CountryController extends Controller {
+/**
+ * CountryController implements the CRUD actions for Country model.
+ */
+class CountryController extends Controller
+{
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
 
-  public function actionIndex() {
+    /**
+     * Lists all Country models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $searchModel = new CountrySearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
 
-    //creates a ActiveQuery query object, which provides methods to access data from the country table.
-    $query = Country::find();
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
 
-    $pagination = new Pagination([
-      'defaultPageSize' => 5,
-      'totalCount' => $query->count(),
-    ]);
+    /**
+     * Displays a single Country model.
+     * @param string $code Code
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($code)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($code),
+        ]);
+    }
 
-    $countries = $query->orderBy('name')
-      ->offset($pagination->offset)
-      ->limit($pagination->limit)
-      ->all();
+    /**
+     * Creates a new Country model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new Country();
 
-    return $this->render('index', [
-      'countries' => $countries,
-      'pagination' => $pagination
-    ]);
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'code' => $model->code]);
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
 
-  }
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
 
+    /**
+     * Updates an existing Country model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $code Code
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($code)
+    {
+        $model = $this->findModel($code);
 
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'code' => $model->code]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Country model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $code Code
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($code)
+    {
+        $this->findModel($code)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Country model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $code Code
+     * @return Country the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($code)
+    {
+        if (($model = Country::findOne(['code' => $code])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
 }
--- models/Country.php
+++ models/Country.php
@@ -1,8 +1,54 @@
 <?php
 
 namespace app\models;
-use yii\db\ActiveRecord;
 
-class Country extends ActiveRecord {
-  
+use Yii;
+
+/**
+ * This is the model class for table "country".
+ *
+ * @property string $code
+ * @property string $name
+ * @property int $population
+ */
+class Country extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['population'], 'default', 'value' => 0],
+            [['code', 'name'], 'required'],
+            [['population'], 'default', 'value' => null],
+            [['population'], 'integer'],
+            [['code'], 'string', 'max' => 2],
+            [['name'], 'string', 'max' => 52],
+            [['code'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
+
 }
